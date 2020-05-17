d15a2985490111f8f098d72a5dc0e6a26baec705
Anik <marufmonem@gmail.com>
2020-05-17 16:53:29 +0600

    Added delete feature

---
+++ hb-02-one-to-one-bi/src/com/hibernate/demo/DeleteInstructorDetailDemo.java
@@ -0,0 +1,57 @@
+package com.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.demo.entity.Instructor;
+import com.hibernate.demo.entity.InstructorDetail;
+
+public class DeleteInstructorDetailDemo {
+
+	public static void main(String[] args) {
+		
+		//create a session factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+		
+		//create a session
+		Session session = factory.getCurrentSession();
+		
+		try {
+
+			//start a transaction
+			session.beginTransaction();
+
+			//get the instructor detail object
+			int theId=8;
+			InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class, theId);
+			
+			if (tempInstructorDetail != null) {
+				
+				//print the instructor detail
+				System.out.println("Delete  instructor detail is : " + tempInstructorDetail);
+				//print the associated instructor
+				System.out.println("Delete the associated instructor is: " + tempInstructorDetail.getInstructor());
+				
+				//delete 
+				session.delete(tempInstructorDetail);
+				
+				//commit transaction
+				session.getTransaction().commit();
+				System.out.println("done");
+			}else {
+				System.out.println("It was null");
+			}
+
+			
+		} finally {
+			session.close(); //handles connection leak
+			factory.close();
+		}
+	}
+
+}
